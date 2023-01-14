8852dd940da44bd415f9000df7692bc4ea27fa10
Алексей <7876440@mail.ru>
2023-01-14 20:38:04 +0300

    uikit(homework)

---
+++ Navigation/ProfileModule/PostProfile.swift
@@ -0,0 +1,8 @@
+//
+//  PostProfile.swift
+//  Navigation
+//
+//  Created by Надежда Данилюк on 14.01.2023.
+//
+
+import Foundation
---
+++ Navigation/ProfileModule/PostTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+//  Created by Надежда Данилюк on 14.01.2023.
+//
+
+import Foundation
